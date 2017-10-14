c6b3c59a6b1db83c5fbb9daccf32bff462ff0dfc
Tracy Kaonje Omamo <tomamo@SamaDc.corp>
2017-10-14 12:42:23 +0300

    function works

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,18 @@
 # A function that checks if a string is a palindrome
 
-def is_palindrome(string)
+def is_palindrome(string):
     if type(string) != str:
         return False
     string = string.upper().replace(' ','')
-    reversed_string = string[::-1]
-    if reversed_string == string:
+    string_1=''
+
+    #list comprehension
+
+    for char in string:
+        if char.isalnum():
+            string_1 += char
+    reversed_string = string_1[::-1]
+    if reversed_string == string_1:
         return True
     return False
 
